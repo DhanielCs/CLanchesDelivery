2614093a8e226ba5437f5682c33045cf9f09986b
DhanielCs <dhaniel010@hotmail.com>
2021-01-26 02:35:19 -0300

    Projeto iniciado backend

---
+++ Backend/src/test/java/com/dh/CLanchesDelivery/CLanchesDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.CLanchesDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CLanchesDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
